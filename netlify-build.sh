4b907722ba62da79048ad01e52fadee13783363f
Xavi <suissejobsolution@gmail.com>
2025-01-06 17:21:46 +0100

    Mise à jour du script pour installer Hugo localement

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Installer Hugo
+# Créer un répertoire local pour Hugo
+mkdir -p $HOME/hugo
+
+# Télécharger Hugo
 wget https://github.com/gohugoio/hugo/releases/download/v0.140.2/hugo_extended_0.140.2_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.140.2_Linux-64bit.tar.gz
-mv hugo /usr/local/bin/
+
+# Extraire Hugo
+tar -xvf hugo_extended_0.140.2_Linux-64bit.tar.gz -C $HOME/hugo
+
+# Ajouter Hugo au PATH
+export PATH=$HOME/hugo:$PATH
 
 # Construire le site Hugo
-hugo
+$HOME/hugo/hugo
